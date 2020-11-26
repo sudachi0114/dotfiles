eefbdf66478e5f18423c99e044553c0e018ca030
sudachi0114 <jnevariste@gmail.com>
2020-11-26 11:27:33 +0900

    do not make emacs backup-file

--- inits/01_basic-setting.el
+++ inits/01_basic-setting.el
@@ -7,3 +7,9 @@
 ;; Tab size 4 / Tab 文字を使わない
 ;; (setq-default tab-width 4 indent-tabs-mode nil)
 
+;; not make backup-file like *~
+(setq make-backup-files nil)
+
+;; not make backup-file like #*#
+(setq auto-save-default nil)
+
