7bc0da7725eecdf1f452d4963b74cc800f5f7895
sudachi0114 <jnevariste@gmail.com>
2020-12-05 13:35:26 +0900

    add theme

--- init.el
+++ init.el
@@ -2,11 +2,10 @@
 ;; package.el
 ;;----------
 (require 'package)
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (package-initialize)
-(unless package-archive-contents (package-refresh-contents))
+
 
 ;;----------
 ;; init-loader split-configration-file
@@ -34,7 +33,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(atom-one-dark-theme solarized-theme auto-complete package-utils go-mode nyan-mode monokai-theme init-loader hiwin)))
+   '(markdown-mode atom-one-dark-theme solarized-theme auto-complete package-utils go-mode nyan-mode monokai-theme init-loader hiwin)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
