ade74598c5e9728a4e342a2a6cdd06d7a847be03
sudachi0114 <jnevariste@gmail.com>
2021-01-22 20:22:42 +0900

    ubuntu 用の設定 initialization file を追加

---
+++ setup.ubuntu.sh
@@ -0,0 +1,9 @@
+ln -s ~/dotfiles/.bashrc.ubuntu ~/.bashrc
+ln -s ~/dotfiles/inits ~/.emacs.d/inits
+ln -s ~/dotfiles/init.el ~/.emacs.d/init.el
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.vimrc.maps ~/.vimrc.maps
+ln -s ~/dotfiles/.screenrc ~/.screenrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+exec $SHELL -l
