7d060758b4cd29e9330a1224497cc56df9206188
sudachi0114 <jnevariste@gmail.com>
2021-01-22 20:23:47 +0900

    コメントを追加

--- setup.ubuntu.sh
+++ setup.ubuntu.sh
@@ -1,9 +1,19 @@
+# bash configration file
 ln -s ~/dotfiles/.bashrc.ubuntu ~/.bashrc
+
+# emacs
 ln -s ~/dotfiles/inits ~/.emacs.d/inits
 ln -s ~/dotfiles/init.el ~/.emacs.d/init.el
+
+# vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.vimrc.maps ~/.vimrc.maps
+
+# tmux (and screen) configration file
 ln -s ~/dotfiles/.screenrc ~/.screenrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# git config
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+
 exec $SHELL -l
