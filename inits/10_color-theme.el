8fe72cfe1c16ea2af834e055a02bf89b611743c3
sudachi0114 <jnevariste@gmail.com>
2021-04-22 15:40:12 +0900

    modify format

--- inits/10_color-theme.el
+++ inits/10_color-theme.el
@@ -2,5 +2,5 @@
 ;; emacs color theme
 ;; ----------
 ;; (load-theme 'monokai t)
-(load-theme 'atom-one-dark t)
-;; (load-theme 'solarized-dark t)
+;; (load-theme 'atom-one-dark t)
+(load-theme 'solarized-dark t)
