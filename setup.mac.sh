517b14c81f2f9751bc6b37d3ab3caf7de6cef311
sudachi0114 <jnevariste@gmail.com>
2021-04-28 14:54:58 +0900

    fix ln bug and refactor

--- setup.mac.sh
+++ setup.mac.sh
@@ -1,10 +1,20 @@
+#!/bin/bash
+
+# bash
 ln -sf ~/projects/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/projects/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/projects/dotfiles/inits ~/.emacs.d/inits
+
+# emacs
+ln -sf ~/projects/dotfiles/inits ~/.emacs.d/
 ln -sf ~/projects/dotfiles/init.el ~/.emacs.d/init.el
+
+# vim
 ln -sf ~/projects/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/projects/dotfiles/.vimrc.maps ~/.vimrc.maps
+
+# tmux (and screen)
 ln -sf ~/projects/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/projects/dotfiles/.tmux.conf ~/.tmux.conf
+
+# git
 ln -sf ~/projects/dotfiles/.gitconfig ~/.gitconfig
-exec $SHELL -l
