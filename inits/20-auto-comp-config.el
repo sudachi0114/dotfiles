24285cbf40fbf76e65810ec7922df8b41398851a
sudachi0114 <jnevariste@gmail.com>
2022-06-28 00:27:14 +0900

    go の設定の見直し

--- inits/20-auto-comp-config.el
+++ inits/20-auto-comp-config.el
@@ -4,8 +4,3 @@
 
 ;; 括弧を自動で補完する
 (electric-pair-mode 1)
-
-
-(require 'go-autocomplete)
-(require 'auto-complete-config)
-(ac-config-default)
---
+++ inits/23-gomode.el
@@ -0,0 +1,19 @@
+;; go get で入れたツールのパスを通す
+(add-to-list 'exec-path (expand-file-name "/Users/sudachi/go/bin/"))
+
+;; go-modeのときlspする
+(add-hook 'go-mode-hook #'lsp-deferred)
+;; yasnippetをインストールしていない or snippetを使わない
+(with-eval-after-load 'lsp-mode
+  (setq lsp-enable-snippet nil))
+
+(defun go-mode-omnibus ()
+  ;; Go code formatting by goimports
+  (setq gofmt-command "goimports")
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  ;; Customize compile command to run go build
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
+  )
+(add-hook 'go-mode-hook 'go-mode-omnibus)
